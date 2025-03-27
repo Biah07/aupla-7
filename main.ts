f95c0306604521b0e3dd8532ecf0b143fb0270e0
Biah07 <00001083356987sp@al.educacao.sp.gov.br>
2025-03-27 11:38:46 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,59 @@
+basic.forever(function () {
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Ode), music.PlaybackMode.InBackground)
+    basic.showLeds(`
+        . # . . .
+        # # # # .
+        # # . . .
+        # . # . .
+        # . . # .
+        `)
+    basic.showLeds(`
+        . . # . .
+        # # # # .
+        . . # . #
+        . . . # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . # . .
+        . # # # #
+        . . # . .
+        . # . # .
+        . # . . #
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # # # #
+        # . # . .
+        . # . . .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # # .
+        . # # . .
+        . . # . .
+        . # # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . # . .
+        # # # . .
+        . . # . .
+        . . # # .
+        . . # . #
+        `)
+})
